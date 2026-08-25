364a38719b52edc2f52e492b270f06c5acd0c616
IsaiasUrb <iurbinar@uteq.edu.ec>
2026-08-25 00:13:40 -0500

    style(mobile): unificar tema visual con la web

--- apps/mobile/app/src/main/java/ec/edu/uteq/scli/mobile/MainActivity.kt
+++ apps/mobile/app/src/main/java/ec/edu/uteq/scli/mobile/MainActivity.kt
@@ -3,15 +3,15 @@ package ec.edu.uteq.scli.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import ec.edu.uteq.scli.mobile.common.navigation.AppNavHost
+import ec.edu.uteq.scli.mobile.common.theme.ScliTheme
 import ec.edu.uteq.scli.mobile.features.notifications.RequestNotificationPermissionEffect
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            ScliTheme {
                 RequestNotificationPermissionEffect()
                 AppNavHost(application as ScliMobileApplication)
             }
---
+++ apps/mobile/app/src/main/java/ec/edu/uteq/scli/mobile/common/theme/Color.kt
@@ -0,0 +1,21 @@
+package ec.edu.uteq.scli.mobile.common.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ScliLightBg = Color(0xFFEDF3F0)
+val ScliLightBgCard = Color(0xFFFFFDF9)
+val ScliLightText = Color(0xFF183A35)
+val ScliLightPrimary = Color(0xFF1F6257)
+val ScliLightBorder = Color(0xFFD9E4DF)
+val ScliLightDanger = Color(0xFFC45142)
+val ScliLightDangerBg = Color(0xFFFFF4F1)
+val ScliLightDangerText = Color(0xFFA4382E)
+
+val ScliDarkBg = Color(0xFF10201C)
+val ScliDarkBgCard = Color(0xFF172B25)
+val ScliDarkText = Color(0xFFE6F0EC)
+val ScliDarkPrimary = Color(0xFF5FB8A4)
+val ScliDarkBorder = Color(0xFF24413A)
+val ScliDarkDanger = Color(0xFFE17A6B)
+val ScliDarkDangerBg = Color(0xFF3A2320)
+val ScliDarkDangerText = Color(0xFFF0A99C)
