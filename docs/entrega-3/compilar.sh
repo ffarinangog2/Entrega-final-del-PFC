ed6b48b7b9c4372c1c3432e57f731196e0f2a3ac
IsaiasUrb <iurbinar@uteq.edu.ec>
2026-09-13 17:27:07 -0500

    docs(e6): congelar snapshot reproducible de entrega 3

---
+++ docs/entrega-3/compilar.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+set -eu
+
+SOURCE_DATE_EPOCH="${SOURCE_DATE_EPOCH:-1786845190}"
+export SOURCE_DATE_EPOCH
+export FORCE_SOURCE_DATE=1
+export TZ=UTC
+
+build_dir="build"
+mkdir -p "$build_dir"
+rm -f "$build_dir/main.aux" "$build_dir/main.bbl" "$build_dir/main.blg" \
+  "$build_dir/main.fdb_latexmk" "$build_dir/main.fls" "$build_dir/main.log" \
+  "$build_dir/main.out" "$build_dir/main.pdf" "$build_dir/main.toc"
+
+pdflatex -interaction=nonstopmode -halt-on-error -recorder \
+  -output-directory="$build_dir" main.tex
+BIBINPUTS=".:${BIBINPUTS:-}" bibtex "$build_dir/main"
+pdflatex -interaction=nonstopmode -halt-on-error -recorder \
+  -output-directory="$build_dir" main.tex
+pdflatex -interaction=nonstopmode -halt-on-error -recorder \
+  -output-directory="$build_dir" main.tex
+cp "$build_dir/main.pdf" Informe_E3_SCLI_RECONSTRUIDO.pdf
